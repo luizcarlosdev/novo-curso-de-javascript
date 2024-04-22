e6117deeb21e9fba835e7cd7b4bd1fa3855fc1f2
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-04-22 14:50:09 -0300

    Update index.js

--- pratica/idades/index.js
+++ pratica/idades/index.js
@@ -1,12 +1,13 @@
 const dataMain = new Date();
 
-const anoNasc = 2007;
-const mesNasc = 12;
-const diaNasc = 24;
+const anoNasc = 2004;
+const mesNasc = 7;
+const diaNasc = 11;
 
 const anoAtual = dataMain.getFullYear();
 const mesAtual = 4;
-const diaAtual = 19;
+const diaAtual = dataMain.getDate();
+console.log(2024 - 2007);
 
 let idade = anoAtual - anoNasc;
 let iF = null;
@@ -20,27 +21,56 @@ function validarIdade() {
     iF = idade;
   }
   if (iF == idade - 1) {
-    mF = (12 - mesNasc) + mesAtual;
-  } else{
+    mF = 12 - mesNasc + mesAtual;
+  } else {
     if (mesNasc > mesAtual) {
       mF = mesNasc - mesAtual;
     } else {
       mF = mesAtual - mesNasc;
     }
   }
-  if (diaNasc != diaAtual) {
-    if (diaNasc > diaAtual) {
-      dF = diaNasc - diaAtual;
-    } else {
-      dF = diaAtual - diaNasc;
+  if (
+    mesNasc - mesAtual == 0 ||
+    (mesAtual - mesNasc == 0 && diaAtual == diaNasc)
+  ) {
+    console.log("Feliz Aniversário!");
+  }
+  if (mesNasc == mesAtual - 1 && diaAtual != diaNasc) {
+    if (mesNasc % 2 == 0) {
+      dF = 30 - diaNasc + diaAtual;
+      if (dF > 30) {
+        dF = 30 - dF;
+        mF++;
+      }
+    } else if (mesNasc % 2 != 0) {
+      dF = 30 - diaNasc + diaAtual;
+      if (dF > 30) {
+        dF = (30 - dF) * -1;
+        mF++;
+      }
     }
   }
+  if (mesNasc == mesAtual - 1 && diaAtual == diaNasc) {
+    dF = 0;
+  }
+  if (mesNasc == mesAtual - 1 && diaAtual < diaNasc) {
+    dF = diaNasc - diaAtual;
+  }
+  if (mesNasc != mesAtual - 1 && diaAtual > diaNasc) {
+    dF = diaAtual - diaNasc;
+  }
+  if (mesNasc != mesAtual - 1 && diaAtual < diaNasc) {
+    dF = diaNasc - diaAtual;
+  }
   if (mF > 12) {
-    mF = (12 - mF)*(-1);
+    mF = (12 - mF) * -1;
     iF++;
   }
+  iF = iF < 10 ? "0" + iF : iF;
+  mF = mF < 10 ? "0" + mF : mF;
+  dF = dF < 10 ? "0" + dF : dF;
 
-  return `Você tem ${iF} anos,${+mF} meses e ${dF} dias`;
+  return `Você tem ${iF} anos,${mF} meses e ${dF} dias`;
 }
 
 console.log(validarIdade());
